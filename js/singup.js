8f6ea295d3f753ad83b646b255f201b02e4e689f
RodrigoLMF <rodrigoluizmf@gmail.com>
2023-10-30 20:26:46 -0300

    adicionado script signup.js

---
+++ js/singup.js
@@ -0,0 +1,26 @@
+document.getElementById("btnCadastrar").addEventListener("click", function() {
+    var login = document.getElementById("login").value;
+    var password = document.getElementById("password").value;
+
+    var userData = {
+        login: login,
+        password: password
+    };
+
+    fetch('/cadastrarUsuario', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(userData)
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        alert(data.message); 
+    })
+    .catch(error => {
+        console.error('Erro ao cadastrar usuário:', error);
+        alert('Erro ao cadastrar usuário. Por favor, tente novamente mais tarde.');
+    });
+});
