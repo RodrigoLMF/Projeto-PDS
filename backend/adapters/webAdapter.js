222fbc2c50cbc6352251fbb73adbd4f5982c74c6
EmyleSantos <emylesantoslima@gmail.com>
2023-10-30 11:09:04 -0300

    update rotas

--- backend/adapters/webAdapter.js
+++ backend/adapters/webAdapter.js
@@ -3,12 +3,17 @@ const billDomain = require('../domain/Bill/billDomain.js');
 const express = require('express');
 const app = express();
 
+const userRouter = require('./userControllers');
+
 function start() {
-    app.use(express.static('public'));
-    
-    app.get('/', (req, res) => {
-        res.send('Página inicial')
-    });
+    //app.use(express.static('public'));
+    app.use('/', userRouter);
+
+    // app.get('/', (req, res) => {
+    //     res.send('Página inicial')
+    // });
+
+    app.use('/cadastrarUsuario', userRouter);
 
     const PORT = 3000;
     app.listen(PORT, '0.0.0.0', () => {
