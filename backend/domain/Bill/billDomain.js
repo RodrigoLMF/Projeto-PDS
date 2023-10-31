2674a365286f342846b4be43e7605ae9f3a81ba5
EmyleSantos <emylesantoslima@gmail.com>
2023-10-31 01:14:41 -0300

    criando funções para cadastrar conta

--- backend/domain/Bill/billDomain.js
+++ backend/domain/Bill/billDomain.js
@@ -1,12 +1,42 @@
 // Código de domínio (regras de negócio)
 // Totalmente independente de tecnologia
+var Bill = require('./Bill.js').Bill
 
-var repositorio = null;
+var repository = null;
 
 function configRepo(repo) {
-  repositorio = repo; 
+    repository = repo;
+}
+
+function splitBill() {
+
+}
+
+function repeatBill() {
+
+}
+
+function registerBill(userID, name, value, type, divide, repeat, num_parts, first_payment, payday) {
+    // resolver userID depois
+    newBill = new Bill(0, userID, name, value, type, divide, repeat, num_parts, first_payment, payday)
+    return new Promise((resolve, reject) => {
+        if (divide) {  // resolver depois
+            splitBill()
+        } else if (repeat) {  // resolver depois
+            repeatBill()
+        } else {
+            repository.add(newBill).then((result) => {
+                //newBill.setId = result.id
+                resolve({ message: 'Usuário cadastrado com sucesso!' });
+            }).catch((err) => {
+                console.log("Erro ao cadastrar conta:", err);
+                reject(new Error('Erro ao cadastrar conta. Por favor, tente novamente mais tarde.'));
+            });
+        }
+
+    });
 }
 
 
-
 module.exports.configRepo = configRepo;
+module.exports.registerBill = registerBill;
