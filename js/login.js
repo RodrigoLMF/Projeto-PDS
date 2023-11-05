5f8e4359692716b49153be3ede1c0efedb6eed26
RodrigoLMF <rodrigoluizmf@gmail.com>
2023-11-05 18:23:49 -0300

    adicionado script login.js

---
+++ js/login.js
@@ -0,0 +1,27 @@
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault(); 
+  
+    const login = document.getElementById('login').value;
+    const password = document.getElementById('password').value;
+  
+    fetch('/api/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ login, password })
+    })
+    .then(response => response.json())
+    .then(data => {
+      if (data.message === 'Login bem-sucedido') {
+        window.location.href = '/dashboard.html'; 
+      } else {
+        alert('Credenciais inválidas. Por favor, tente novamente.');
+      }
+    })
+    .catch(error => {
+      console.error('Erro:', error);
+      alert('Erro ao fazer login. Por favor, tente novamente mais tarde.');
+    });
+  });
+  
