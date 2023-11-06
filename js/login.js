328d8a23c0d813dea22d96ccc53487e5f91b85ac
RodrigoLMF <rodrigoluizmf@gmail.com>
2023-11-06 12:31:23 -0300

    alterado login

--- js/login.js
+++ js/login.js
@@ -1,26 +1,27 @@
 document.getElementById('loginForm').addEventListener('submit', function(event) {
-  event.preventDefault(); 
-
-  const login = document.getElementById('login').value;
-  const password = document.getElementById('password').value;
-
-  fetch('/api/login', {
+    event.preventDefault(); 
+  
+    const login = document.getElementById('login').value;
+    const password = document.getElementById('password').value;
+  
+    fetch('/api/login', {
       method: 'POST',
       headers: {
-          'Content-Type': 'application/json'
+        'Content-Type': 'application/json'
       },
       body: JSON.stringify({ login, password })
-  })
-  .then(response => response.json())
-  .then(data => {
+    })
+    .then(response => response.json())
+    .then(data => {
       if (data.message === 'Login bem-sucedido') {
-          window.location.href = 'html/homepage.html'; // Redireciona para homepage.html
+        window.location.href = 'html/homepage.html'; 
       } else {
-          alert('Credenciais inválidas. Por favor, tente novamente.');
+        alert('Credenciais inválidas. Por favor, tente novamente.');
       }
-  })
-  .catch(error => {
+    })
+    .catch(error => {
       console.error('Erro:', error);
       alert('Erro ao fazer login. Por favor, tente novamente mais tarde.');
+    });
   });
-});
+  
