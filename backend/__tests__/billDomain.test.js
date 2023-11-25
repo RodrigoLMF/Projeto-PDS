a329322892c4eb451c93096bc501fd6eff358897
Igor Roiz <igorrt66@gmail.com>
2023-11-25 10:47:20 -0300

    Testes para getBillsWithinPeriod, getBillsWithinPeriodgetAllBillsByUserId e

--- backend/__tests__/billDomain.test.js
+++ backend/__tests__/billDomain.test.js
@@ -197,3 +197,121 @@ describe('billDomain registerBill', () => {
       expect(userRepository.getAllBillsByUserId).toHaveBeenCalledWith(userId);
     });
   });
+
+  describe('billDomain getBillsWithinPeriod', () => {
+    let userRepository;
+  
+    beforeEach(() => {
+      userRepository = {
+        getBillsWithinPeriod: jest.fn(),
+      };
+      billDomain.configRepo(userRepository);
+    });
+  
+    test('Deve obter somatório de contas do tipo específico dentro do período com sucesso', async () => {
+      const userId = 1;
+      const type = 'fixa';
+      const startDate = new Date('2023-01-01');
+      const endDate = new Date('2023-01-31');
+      const mockBills = [
+        new Bill(1, userId, 'Conta de Luz', 100, 'fixa', false, true, 3, new Date('2023-01-05'), new Date('2023-01-10')),
+        new Bill(2, userId, 'Conta de Água', 50, 'variavel', false, true, 3, new Date('2023-01-15'), new Date('2023-01-20')),
+      ];
+  
+      userRepository.getBillsWithinPeriod.mockResolvedValue(mockBills);
+  
+      const result = await billDomain.getParcialBalanceWithinPeriod(userId, type, startDate, endDate);
+  
+      expect(result).toEqual(100); // Soma de Conta de Luz (100)
+      expect(userRepository.getBillsWithinPeriod).toHaveBeenCalledWith(userId, startDate, endDate);
+    });
+  
+    test('Deve lidar com erro ao obter somatório de contas do tipo específico dentro do período', async () => {
+      const userId = 1;
+      const type = 'fixa';
+      const startDate = new Date('2023-01-01');
+      const endDate = new Date('2023-01-31');
+      const errorMessage = 'Erro ao buscar contas parciais. Por favor, tente novamente mais tarde.';
+      userRepository.getBillsWithinPeriod.mockRejectedValue(new Error(errorMessage));
+  
+      await expect(billDomain.getParcialBalanceWithinPeriod(userId, type, startDate, endDate)).rejects.toThrow(errorMessage);
+      expect(userRepository.getBillsWithinPeriod).toHaveBeenCalledWith(userId, startDate, endDate);
+    });
+  });
+
+  describe('billDomain getAllBillsByUserId', () => {
+    let userRepository;
+  
+    beforeEach(() => {
+      userRepository = {
+        getAllBillsByUserId: jest.fn(),
+      };
+      billDomain.configRepo(userRepository);
+    });
+  
+    test('Deve obter somatório de contas do tipo "G" ou "D" com sucesso', async () => {
+      const userId = 1;
+      const mockBills = [
+        new Bill(1, userId, 'Receita', 500, 'G', false, true, 1, new Date('2023-01-01'), new Date('2023-01-05')),
+        new Bill(2, userId, 'Despesa', 200, 'D', false, true, 1, new Date('2023-01-10'), new Date('2023-01-15')),
+        new Bill(3, userId, 'Conta de Luz', 100, 'fixa', false, true, 3, new Date('2023-01-20'), new Date('2023-01-25')),
+      ];
+  
+      userRepository.getAllBillsByUserId.mockResolvedValue(mockBills);
+  
+      const result = await billDomain.getTotalBalanceAll(userId);
+  
+      expect(result).toEqual(700); // Soma de Receita (500) + Despesa (200)
+      expect(userRepository.getAllBillsByUserId).toHaveBeenCalledWith(userId);
+    });
+  
+    test('Deve lidar com erro ao obter somatório de contas do tipo "G" ou "D"', async () => {
+      const userId = 1;
+      const errorMessage = 'Erro ao buscar contas parciais. Por favor, tente novamente mais tarde.';
+      userRepository.getAllBillsByUserId.mockRejectedValue(new Error(errorMessage));
+  
+      await expect(billDomain.getTotalBalanceAll(userId)).rejects.toThrow(errorMessage);
+      expect(userRepository.getAllBillsByUserId).toHaveBeenCalledWith(userId);
+    });
+
+  });
+
+  describe('billDomain getBillsWithinPeriod', () => {
+    let userRepository;
+  
+    beforeEach(() => {
+      userRepository = {
+        getBillsWithinPeriod: jest.fn(),
+      };
+      billDomain.configRepo(userRepository);
+    });
+  
+    test('Deve obter somatório de contas do tipo "G" ou "D" dentro do período com sucesso', async () => {
+      const userId = 1;
+      const startDate = new Date('2023-01-01');
+      const endDate = new Date('2023-01-31');
+      const mockBills = [
+        new Bill(1, userId, 'Receita', 500, 'G', false, true, 1, new Date('2023-01-05'), new Date('2023-01-10')),
+        new Bill(2, userId, 'Despesa', 200, 'D', false, true, 1, new Date('2023-01-15'), new Date('2023-01-20')),
+        new Bill(3, userId, 'Conta de Luz', 100, 'fixa', false, true, 3, new Date('2023-02-01'), new Date('2023-02-05')),
+      ];
+  
+      userRepository.getBillsWithinPeriod.mockResolvedValue(mockBills);
+  
+      const result = await billDomain.getTotalBalanceWithinPeriod(userId, startDate, endDate);
+  
+      expect(result).toEqual(700); // Soma de Receita (500) + Despesa (200)
+      expect(userRepository.getBillsWithinPeriod).toHaveBeenCalledWith(userId, startDate, endDate);
+    });
+  
+    test('Deve lidar com erro ao obter somatório de contas do tipo "G" ou "D" dentro do período', async () => {
+      const userId = 1;
+      const startDate = new Date('2023-01-01');
+      const endDate = new Date('2023-01-31');
+      const errorMessage = 'Erro ao buscar contas parciais. Por favor, tente novamente mais tarde.';
+      userRepository.getBillsWithinPeriod.mockRejectedValue(new Error(errorMessage));
+  
+      await expect(billDomain.getTotalBalanceWithinPeriod(userId, startDate, endDate)).rejects.toThrow(errorMessage);
+      expect(userRepository.getBillsWithinPeriod).toHaveBeenCalledWith(userId, startDate, endDate);
+    });
+  });
