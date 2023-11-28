f3ff613a5c37db4833940bbf9a9824c0e70f8d54
Igor Roiz <igorrt66@gmail.com>
2023-11-28 10:15:54 -0300

    Create jest.config.js

---
+++ backend/jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    testEnvironment: 'node', // Pode ser 'jsdom' para projetos do lado do cliente
+    roots: ['<rootDir>/backend/__tests__'],
+    testMatch: ['**/*.test.js'], // Padrão dos arquivos de teste
+    // Outras configurações do Jest, se necessário
+  };
+  
